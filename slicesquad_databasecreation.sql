aa9b50e5e593c0b5dd0e10a4f37aed0fe801cab7
Andrew Worthington <121584287+adotworth@users.noreply.github.com>
2023-02-12 12:47:06 -0800

    Add files via upload

---
+++ slicesquad_databasecreation.sql
@@ -0,0 +1,13 @@
+-- Database: slicesquad_data
+
+-- DROP DATABASE IF EXISTS slicesquad_data;
+
+CREATE DATABASE slicesquad_data
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
